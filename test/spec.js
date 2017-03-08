74f7c8e2f199893d0e1eda6c6c30eb8b42ecde80
Daniello <d_crush@hotmail.com>
2017-03-08 17:35:39 +0100

    home view begin e2e test

--- test/spec.js
+++ test/spec.js
@@ -1,11 +1,31 @@
-describe('Get complaint 0', function() {
+describe('home view', function() {
 
-    beforeEach(function() {
-      browser.get('http://localhost:8000/#/complaints/0');
+
+    it('go to detail cafe', function() {
+        browser.get('localhost/8000/');
+
+        //
+        // var selectDropdownbyNum = function ( dropdown, "1" ) {
+        //     if (optionNum){
+        //         var options = element.findElements(by.tagName('option'))
+        //             .then(function(options){
+        //                 options[optionNum].click();
+        //             });
+        //     }
+        // };
+
+        element(by.cssContainingText('dropdown', 'Amsterdam')).click();
+
+        element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+        element(by.css('[value="add"]')).click();
+
+        var todoList = element.all(by.repeater('todo in todoList.todos'));
+        expect(todoList.count()).toEqual(3);
+        expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+        // You wrote your first test, cross it off the list
+        todoList.get(2).element(by.css('input')).click();
+        var completedAmount = element.all(by.css('.done-true'));
+        expect(completedAmount.count()).toEqual(2);
     });
-
-
-    it('should display page id 0', function() {
-      expect(element(by.binding('id')).getText()).toBe('0');
-    });
-  });
+});
