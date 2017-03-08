6e361103a6f67d63b2d73f9302a843c3c2d08079
Frizky Kramer <frizkykramer@gmail.com>
2017-03-08 16:44:59 +0100

    e2e test voor cities

---
+++ test/e2e/protractor.conf.js
@@ -0,0 +1,24 @@
+//jshint strict: false
+exports.config = {
+
+  allScriptsTimeout: 11000,
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  
+  specs: [
+    'scenarios.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8000/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+
+};
